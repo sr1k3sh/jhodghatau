2b2fe9f755ac848219fc476d7e9d47a2597f5883
subooom <subhamkharel12@gmail.com>
2019-10-01 23:02:29 +0545

    Navbar and basic layout

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './navbar.css'
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div className="top_panel top_panel_custom top_panel_custom_14 top_panel_custom_header-burger-menu without_bg_image">
+        test
+      </div>
+    )
+  }
+}
